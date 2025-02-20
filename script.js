bd13fdbdeaa39872e6bd2f571d615fb611ced9c9
Mfon-obong Umoh <mfonobongumoh75@gmail.com>
2025-02-20 22:55:53 +0100

    changes

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 
-let loginForm = document.getElementById('loginForm');
+const loginForm = document.getElementById('loginForm');
+const submitBtn = document.getElementById('submitbtn');
+const handleUserNameError = document.getElementById('handleUserNameError');
+const handleUserPasswordError = document.getElementById('handlePasswordError');
+const handleBothError = document.getElementById('handleBothError')
 
-loginForm.addEventListener('submit', ()=>{
-    let userInput = document.getElementById('username').value;
-    let userPassword = document.getElementById('password').value;
+loginForm.addEventListener('submit', (e)=>{
+        e.preventDefault();
+    
+    const userInput = document.getElementById('username').value;
+    const userPassword = document.getElementById('password').value;
 
-    if(userInput === '' || userPassword === ''){
-        alert('Both fields are required!')
+    if(!userInput){
+        handleUserNameError.innerHTML='Please fill in your Username!';
+    }if(!userPassword){
+        handleUserPasswordError.innerHTML='Please fill in your password!'
+    }if(userInput === '' && userPassword === ''){
+        handleBothError.innerHTML='Empty fields detected. please fill in all required fields to proceed'
+
     }
 });
 
