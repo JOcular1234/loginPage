616dcd944485e9b028f342b25c0a31f0b29182da
Mfon-obong Umoh <mfonobongumoh75@gmail.com>
2025-02-22 00:06:56 +0100

    update

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const submitBtn = document.getElementById('submitbtn');
 const handleUserNameError = document.getElementById('handleUserNameError');
 const handleUserPasswordError = document.getElementById('handlePasswordError');
 const handleBothError = document.getElementById('handleBothError')
+let password = document.getElementById('password');
+let revealPassword = document.getElementById('eye');
+
 
 loginForm.addEventListener('submit', (e)=>{
         e.preventDefault();
@@ -25,3 +28,13 @@ loginForm.addEventListener('submit', (e)=>{
 document.getElementById('menu-toggle').addEventListener('click', function() {
     document.getElementById('menu').classList.toggle('active');
 });
+
+function hide() {
+    if (password.type == 'password') {
+        password.type = 'text';
+        revealPassword.className = 'fa-solid fa-eye-slash';
+    }else{
+        password.type = 'password';
+        revealPassword.className = 'fa-solid fa-eye'
+    }
+}
